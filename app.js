d72b4c10120879496a1e8897c52988eaf811ccb9
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2024-04-19 17:54:30 +0530

    port changed

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(3000, () => {
-  console.log("Listening to 3000 port");
+app.listen(80, () => {
+  console.log("Listening to 80 port");
 });
